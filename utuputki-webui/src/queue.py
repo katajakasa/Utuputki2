509c174325482ae61207ed31011e008b86408fe5
Tuomas Virtanen <katajakasa@gmail.com>
2015-10-16 10:18:21 +0300

    Celery updates

--- utuputki-webui/src/queue.py
+++ utuputki-webui/src/queue.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from celery import Celery
+import settings
 
 
 def queue_init():
-    app = Celery('tasks')
-    app.config_from_object('celery_config')
+    app = Celery('tasks',
+                 backend=settings.CELERY_BACKEND,
+                 broker=settings.CELERY_BROKER)
+    app.conf.update(
+        CELERY_TASK_SERIALIZER='json',
+        CELERY_ACCEPT_CONTENT=['json'],
+        CELERY_RESULT_SERIALIZER='json',
+        CELERY_TIMEZONE=settings.TIMEZONE,
+        CELERY_ENABLE_UTC=True,
+    )
 
